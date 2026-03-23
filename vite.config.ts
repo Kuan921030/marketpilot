070a2f4f41b1d0ab1f37eae55dcb461780c91c88
KuanCH <113124817+Kuan921030@users.noreply.github.com>
2026-03-23 14:45:45 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // Read env vars at build time
   const finnhubKey = process.env.VITE_FINNHUB_KEY || '';
 
